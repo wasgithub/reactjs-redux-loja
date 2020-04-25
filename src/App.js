e5ccbfa6ee4108f3801d48dd8caf8a39f0b70c53
Washington Alexandre da Silva <washington.silva@totvs.com.br>
2020-04-25 20:22:04 -0300

    HEADER: estilizado reader

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
 
 import GlobalStyles from './styles/globals';
+import Header from './components/Header';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes />
       <GlobalStyles />
     </BrowserRouter>
---
+++ src/components/Header/styles.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 50px 0;
+`;
+
+export const Cart = styled(Link)`
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  transition: opacity 0.3s;
+
+  &:hover {
+    opacity: 0.7;
+  }
+
+  div {
+    text-align: right;
+    margin-right: 10px;
+
+    strong {
+      display: block;
+      color: #fff;
+    }
+
+    span {
+      color: #999;
+      font-size: 12;
+    }
+  }
+`;
