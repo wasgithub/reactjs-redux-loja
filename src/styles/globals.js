a634c1c88ddc152dc5453c8756354d354060f659
Washington Alexandre da Silva <washington.silva@totvs.com.br>
2020-04-25 18:51:38 -0300

    STYLES: adicionado estilos/ fonts/ images

---
+++ src/styles/globals.js
@@ -0,0 +1,37 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  html, body, #root {
+    min-height: 100%
+  }
+
+  body {
+    background: #191919;
+    -webkit-font-smoothing: antialiased !important;
+  }
+
+  body, input, button {
+    color: #222;
+    font-size: 14px;
+    font-family: Roboto, Helvetica, sans-serif;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  #root {
+    max-width: 1020px;
+    margin: 0 auto;
+    padding: 0 20px 50px;
+  }
+
+`;
