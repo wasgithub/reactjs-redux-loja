ea4817408817287c153bdc8b6c5462039539cd3e
Washington Alexandre da Silva <washington.silva@totvs.com.br>
2020-04-25 23:02:27 -0300

    HOME/polished : ajustado tela home/

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,104 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
+
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <div>teste</div>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-hera-feminino/08/D22-3305-008/D22-3305-008_detalhe1.jpg?ts=1574361288?ims=280x280"
+          alt="Tênis"
+        />
+        <strong>
+          Tênis muito legal com um titulo muito grande Tênis muito legal com um
+          titulo muito grande
+        </strong>
+        <span>R$ 129,00</span>
+
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-hera-feminino/08/D22-3305-008/D22-3305-008_detalhe1.jpg?ts=1574361288?ims=280x280"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,00</span>
+
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-hera-feminino/08/D22-3305-008/D22-3305-008_detalhe1.jpg?ts=1574361288?ims=280x280"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,00</span>
+
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-hera-feminino/08/D22-3305-008/D22-3305-008_detalhe1.jpg?ts=1574361288?ims=280x280"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,00</span>
+
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-hera-feminino/08/D22-3305-008/D22-3305-008_detalhe1.jpg?ts=1574361288?ims=280x280"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,00</span>
+
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-hera-feminino/08/D22-3305-008/D22-3305-008_detalhe1.jpg?ts=1574361288?ims=280x280"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,00</span>
+
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
