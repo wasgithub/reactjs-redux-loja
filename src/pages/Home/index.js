b1326195c2b68e60137fd20a35c1767d67619a14
Washington Alexandre da Silva <washington.silva@totvs.com.br>
2020-04-30 20:23:25 -0300

    FORMA-CURRENCY: Intl.numberformart

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,104 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+import api from '../../services/api';
+import { formatPrice } from '../../utils/format';
 
 import { ProductList } from './styles';
 
 export default function Home() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    setProductsService();
+  }, []);
+
+  async function setProductsService() {
+    try {
+      const response = await api.get('products');
+      const data = response.data.map((product) => ({
+        ...product,
+        formatPrice: formatPrice(product.price),
+      }));
+
+      setProducts(data);
+    } catch (error) {}
+  }
+
   return (
     <ProductList>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-olympikus-hera-feminino/08/D22-3305-008/D22-3305-008_detalhe1.jpg?ts=1574361288?ims=280x280"
-          alt="Tênis"
-        />
-        <strong>
-          Tênis muito legal com um titulo muito grande Tênis muito legal com um
-          titulo muito grande
-        </strong>
-        <span>R$ 129,00</span>
+      {products.map((product) => (
+        <li key={product.id}>
+          <img src={product.image} alt={product.title} />
+          <strong>{product.title}</strong>
+          <span>{product.formatPrice}</span>
 
-        <button>
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-olympikus-hera-feminino/08/D22-3305-008/D22-3305-008_detalhe1.jpg?ts=1574361288?ims=280x280"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$ 129,00</span>
-
-        <button>
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-olympikus-hera-feminino/08/D22-3305-008/D22-3305-008_detalhe1.jpg?ts=1574361288?ims=280x280"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$ 129,00</span>
-
-        <button>
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-olympikus-hera-feminino/08/D22-3305-008/D22-3305-008_detalhe1.jpg?ts=1574361288?ims=280x280"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$ 129,00</span>
-
-        <button>
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-olympikus-hera-feminino/08/D22-3305-008/D22-3305-008_detalhe1.jpg?ts=1574361288?ims=280x280"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$ 129,00</span>
-
-        <button>
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-olympikus-hera-feminino/08/D22-3305-008/D22-3305-008_detalhe1.jpg?ts=1574361288?ims=280x280"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$ 129,00</span>
-
-        <button>
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
+          <button type="button">
+            <div>
+              <MdAddShoppingCart size={16} color="#fff" /> 3
+            </div>
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+        </li>
+      ))}
     </ProductList>
   );
 }
