359d2172485606d7030b78d0d3f7063c7b901ece
Washington Alexandre da Silva <washington.silva@totvs.com.br>
2020-05-01 19:18:10 -0300

    REACTOTRON: Configurado Reactotron

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+
+import './config/ReactotronConfig';
 import Routes from './routes';
 
 import store from './store';
